e19af292dfbc14e6dbbebe6495e1943445ab53e8
JungCheChang <jungche0408@gmail.com>
2023-02-07 16:47:25 -0800

    updated

--- base/urls.py
+++ base/urls.py
@@ -13,7 +13,5 @@ urlpatterns = [
     path("update-room/<str:pk>/", views.updateRoom, name="update-room"),
     path("delete-room/<str:pk>/", views.deleteRoom, name="delete-room"),
     path("delete-message/<str:pk>/", views.deleteMessage, name="delete-message"),
-    
-    
-    path('test/', views.test)
+    path('update-user', views.updateUser, name = "update-user"),
 ]
